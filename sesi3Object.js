05381e9a1ce3beb7e776aad49de6d0b8912d0aea
mRzz129 <114788470+mRzz129@users.noreply.github.com>
2023-10-20 17:48:22 +0700

    Add files via upload

---
+++ sesi3Object.js
@@ -0,0 +1,14 @@
+const restaurant = {
+    name: "GP",
+    city: "Sukabumi",
+    favoriteDrink: "Tequila",
+    favoriteFood: "Nasi goreng kambing",
+    isVegan: false,
+  };
+  
+  const { name, city, favoriteDrink, favoriteFood, isVegan } = restaurant;
+  console.log("Nama : " + name);
+  console.log("Kota : " + city);
+  console.log("Minuman Favorit : " + favoriteDrink);
+  console.log("Makanan Favorit : " + favoriteFood);
+  console.log("Vegan : " + isVegan);
